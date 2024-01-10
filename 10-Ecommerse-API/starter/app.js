01ac906208be264f32f52bfd579717e1989aa7ea
NirwanGupta <guptanirwan@gmail.com>
2024-01-10 19:56:45 +0530

    orders completed

--- 10-Ecommerse-API/starter/app.js
+++ 10-Ecommerse-API/starter/app.js
@@ -13,6 +13,7 @@ const authRouter = require(`./router/authRouter`);
 const userRouter = require(`./router/userRoutes`);
 const productRouter = require(`./router/productRoutes`);
 const reviewRouter = require(`./router/reviewRoutes`);
+const orderRouter = require(`./router/orderRouter`);
 
 const errorHandlerMiddleware = require(`./middleware/error-handler`);
 const notFoundMiddleware = require(`./middleware/not-found`);
@@ -38,6 +39,7 @@ app.use(`/api/v1/auth`, authRouter);
 app.use('/api/v1/users', userRouter);
 app.use(`/api/v1/products`, productRouter);
 app.use(`/api/v1/reviews`, reviewRouter);
+app.use(`/api/v1/orders`, orderRouter);
 
 app.use(notFoundMiddleware);    //  if we try to access routes that are not covered we want 404, and once we reach 404, we want all functionality to stop, thus no next() in NotFoiund
 app.use(errorHandlerMiddleware);
